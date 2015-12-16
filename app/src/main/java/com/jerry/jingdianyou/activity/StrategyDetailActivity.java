c2f182632dc62993ed0f5fe4cf4ff1b15008b567
jerrylost <cecszj@163.com>
2015-12-16 17:34:08 +0800

    修改景点收藏

--- app/src/main/java/com/jerry/jingdianyou/activity/StrategyDetailActivity.java
+++ app/src/main/java/com/jerry/jingdianyou/activity/StrategyDetailActivity.java
@@ -38,7 +38,8 @@ import java.util.Map;
  * Created by Jerry.Zou
  */
 @ContentView(R.layout.strategy_detail)
-public class StrategyDetailActivity extends BaseActivity implements View.OnTouchListener {
+public class StrategyDetailActivity extends BaseActivity implements View.OnTouchListener
+{
   //图片
   @ViewInject(R.id.item_picture)
   private ImageView mStrategyImage;
@@ -83,7 +84,8 @@ public class StrategyDetailActivity extends BaseActivity implements View.OnTouch
   private String title;
 
   @Override
-  protected void onCreate(Bundle savedInstanceState) {
+  protected void onCreate(Bundle savedInstanceState)
+  {
     super.onCreate(savedInstanceState);
     ViewUtils.inject(this);
 
@@ -115,20 +117,25 @@ public class StrategyDetailActivity extends BaseActivity implements View.OnTouch
   }
 
   //加载数据
-  private void initDetailLoading() {
+  private void initDetailLoading()
+  {
 
     params.put("RequestJson", "{\"guides_id\":\"" + id + "\",\"member_id\":\"\"}");
-    JDYHttpConnect.getInstance().getStrategyDetail(params, new DataCallBack() {
+    JDYHttpConnect.getInstance().getStrategyDetail(params, new DataCallBack()
+    {
       @Override
-      public void onSuccess(String response) {
+      public void onSuccess(String response)
+      {
         Gson gson = new Gson();
         strategyDetail = gson.fromJson(response, StrategyDetail.class);
         mStrategyImage.setScaleType(ImageView.ScaleType.FIT_XY);
         mList.clear();
         //动态添加天数
         int size = strategyDetail.getMydata().getGuides_info_data().size();
-        if (size > 1) {
-          for (int i = 1; i <= size; i++) {
+        if (size > 1)
+        {
+          for (int i = 1; i <= size; i++)
+          {
             TextView mTextView = new TextView(getApplicationContext());
             mTextView.setTextSize(25);
             mTextView.setPadding(20, 20, 20, 20);
@@ -137,13 +144,16 @@ public class StrategyDetailActivity extends BaseActivity implements View.OnTouch
             mLinearLayout.addView(mTextView);
           }
         }
-        if (size != 0) {
-          for (int i = 0; i < size; i++) {
+        if (size != 0)
+        {
+          for (int i = 0; i < size; i++)
+          {
             data =
                 strategyDetail.getMydata().getGuides_info_data().get(i).getData();
             int size1 = strategyDetail.getMydata().getGuides_info_data().get(i).getData().size();
 
-            for (int a = 0; a < size1; a++) {
+            for (int a = 0; a < size1; a++)
+            {
               //经度
               position_x = data.get(a).getPosition_x();
 
@@ -159,14 +169,16 @@ public class StrategyDetailActivity extends BaseActivity implements View.OnTouch
       }
 
       @Override
-      public void onFailure(String error) {
+      public void onFailure(String error)
+      {
 
       }
     });
   }
 
   //定位
-  public void onPosition(View view) {
+  public void onPosition(View view)
+  {
     Intent intent = new Intent(this, BaiduMapActivity.class);
     intent.putExtra("position_x", position_x);
     intent.putExtra("position_y", position_y);
@@ -174,16 +186,20 @@ public class StrategyDetailActivity extends BaseActivity implements View.OnTouch
   }
 
   //点击收藏
-  public void onCollect(View view) {
+  public void onCollect(View view)
+  {
     //判断是否已经收藏
     boolean simpleData = jingDbHelper.isSimpleData(
         JingDbTable.JingControll.COLUMN_ID + "=?",
         new String[]{id});
 
-    if (simpleData) {
+    if (simpleData)
+    {
       Toast.makeText(StrategyDetailActivity.this, "数据已经存在，不用再收藏了", Toast.LENGTH_SHORT).show();
 
-    } else {
+    }
+    else
+    {
       //如果不存在，则插入
       ContentValues values = new ContentValues();
       //插入标题
@@ -192,7 +208,8 @@ public class StrategyDetailActivity extends BaseActivity implements View.OnTouch
       values.put(JingDbTable.JingControll.COLUMN_TITLE, title);
       values.put(JingDbTable.JingControll.COLUMN_PIC, pic);
       boolean saveData = new JingDbHelper(this).SaveData(values);
-      if (saveData) {
+      if (saveData)
+      {
         Toast.makeText(this, "收藏成功！！！", Toast.LENGTH_SHORT).show();
       }
     }
@@ -200,36 +217,43 @@ public class StrategyDetailActivity extends BaseActivity implements View.OnTouch
 
   }
 
-  public void doBack(View view) {
+  public void doBack(View view)
+  {
     this.finish();
     this.overridePendingTransition(R.anim.in_from_left, R.anim.out_from_left);
   }
 
   @Override
-  public void onBackPressed() {
+  public void onBackPressed()
+  {
     this.finish();
     this.overridePendingTransition(R.anim.in_from_left, R.anim.out_from_left);
   }
 
   @Override
-  protected void onDestroy() {
+  protected void onDestroy()
+  {
     super.onDestroy();
     mList.clear();
   }
 
   @Override
-  public boolean onTouch(View v, MotionEvent event) {
+  public boolean onTouch(View v, MotionEvent event)
+  {
     float y = 0;
     float v1 = 0;
-    switch (event.getAction()) {
+    switch (event.getAction())
+    {
 
       case MotionEvent.ACTION_UP:
         float y1 = event.getY();
         a = ((1000 - y1) / 800);
-        if (a > 0 && a < 1) {
+        if (a > 0 && a < 1)
+        {
           mRelativeLayout.setAlpha(a);
         }
-        if (a >= 1) {
+        if (a >= 1)
+        {
           mRelativeLayout.setAlpha(1);
         }
         mRelativeLayout.invalidate();
@@ -238,10 +262,12 @@ public class StrategyDetailActivity extends BaseActivity implements View.OnTouch
       case MotionEvent.ACTION_MOVE:
         y1 = event.getY();
         a = ((1000 - y1) / 800);
-        if (a > 0 && a < 1) {
+        if (a > 0 && a < 1)
+        {
           mRelativeLayout.setAlpha(a);
         }
-        if (a >= 1) {
+        if (a >= 1)
+        {
           mRelativeLayout.setAlpha(1);
         }
         mRelativeLayout.invalidate();
